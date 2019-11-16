fd86200cbe2b8bfa28da8dc0c2ae681705cd50d7
axiomaabsurdo <m.mortara01@gmail.com>
2019-11-16 09:34:17 -0300

    test site

---
+++ serveWeb.js
@@ -0,0 +1,11 @@
+var http = require('http')
+var fs = require('fs')
+var server = http.createServer( (req, res) =>{ 
+    console.log('reques made', req.url)
+    res.writeHead(200, {'Content-Type': 'text/html'})
+    var myReadStream = fs.createReadStream(__dirname + './index.html', 'utf8')
+    myReadStream.pipe(res)
+})
+
+server.listen(3000, '127.0.0.1')
+console.log('server is up...')
